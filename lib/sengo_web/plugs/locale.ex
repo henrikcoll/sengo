695f8dd4c5dbdbfc0359192dcd479a93a3738f9d
Henrik Næss <henrik@heka.no>
2021-12-14 16:27:10 +0100

    Set locale in cookie instead of session, and fix Norwegian translation of Register

--- lib/sengo_web/plugs/locale.ex
+++ lib/sengo_web/plugs/locale.ex
@@ -1,16 +1,40 @@
 defmodule SengoWeb.Plugs.Locale do
+  @moduledoc """
+  Plug to set the user's preferred language
+  """
+
   import Plug.Conn
 
   def init(opts), do: opts
 
   def call(conn, _opts) do
-    case conn.params["locale"] || get_session(conn, :locale) do
+    IO.puts(get_locale(conn))
+
+    case get_locale(conn) do
       nil ->
         conn
 
       locale ->
-        Gettext.put_locale(SengoWeb.Gettext, locale)
-        put_session(conn, :locale, locale)
+        update_gettext(locale)
+        update_cookie(conn, locale)
     end
+  end
+
+  defp get_locale(conn) do
+    conn.req_cookies["locale"] ||
+      conn.params["locale"]
+  end
+
+  defp update_cookie(conn, locale) do
+    if conn.req_cookies["locale"] != locale do
+      put_resp_cookie(conn, "locale", locale)
+    else
+      # Do nothing
+      conn
+    end
+  end
+
+  defp update_gettext(locale) do
+    Gettext.put_locale(SengoWeb.Gettext, locale)
   end
 end
